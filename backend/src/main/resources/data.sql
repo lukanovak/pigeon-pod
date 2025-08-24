7eb2650a9120e77515a173ac64c95c457dd8ca84
Asimov <aizhimou@outlook.com>
2025-08-25 07:56:44 +1200

    update default username in user insert statement

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO user (id, username, password, salt, email, role)
 SELECT 0,
-       'admin',
+       'root',
        '3aaf4d8727ead6bcf61512ace7801a4b',
        'fYMtZ=DHj^$G',
        'root@pigeon.pod',
-       'ADMIN' WHERE NOT EXISTS (SELECT 1 FROM user WHERE username = 'admin');
+       'ADMIN' WHERE NOT EXISTS (SELECT 1 FROM user WHERE id = 0);
