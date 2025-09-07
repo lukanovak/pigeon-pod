20b1ce7ee8ca251e30d9a404c498a53877ea338f
Asimov <aizhimou@outlook.com>
2025-09-08 09:26:26 +1200

    feat(ChannelMapper): order channels by last published episode date

--- backend/src/main/java/top/asimov/pigeon/mapper/ChannelMapper.java
+++ backend/src/main/java/top/asimov/pigeon/mapper/ChannelMapper.java
@@ -12,6 +12,7 @@ public interface ChannelMapper extends BaseMapper<Channel> {
   @Select("SELECT c.id, c.handler, c.name, c.avatar_url, c.description, c.channel_source, " +
       "max(e.published_at) as last_published_at " +
       "FROM channel c JOIN episode e ON c.id = e.channel_id " +
-      "GROUP BY c.id, c.handler, c.name, c.avatar_url, c.description, c.channel_source")
+      "GROUP BY c.id, c.handler, c.name, c.avatar_url, c.description, c.channel_source " +
+      "ORDER BY last_published_at DESC")
   List<Channel> selectChannelWithLastUploadedAt();
 }
