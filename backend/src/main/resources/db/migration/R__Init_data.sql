16ee9d31819d0b20f1e9f4ea811660f587e45669
Asimov <aizhimou@outlook.com>
2025-09-20 10:31:25 +1200

    [fix]change the default password to a clear version

--- backend/src/main/resources/db/migration/R__Init_data.sql
+++ backend/src/main/resources/db/migration/R__Init_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO user (id, username, password, salt)
 SELECT 0,
        'root',
-       '3aaf4d8727ead6bcf61512ace7801a4b',
+       'a57b7e53b7253a4ab71eac631647f61f',
        'fYMtZ=DHj^$G'
     WHERE NOT EXISTS (SELECT 1 FROM user WHERE id = 0);
