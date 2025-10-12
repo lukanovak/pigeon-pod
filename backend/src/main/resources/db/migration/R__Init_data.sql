fd03668ea22e71fdb58b5c91a3164a5e692b53bc
Asimov <aizhimou@outlook.com>
2025-10-12 21:11:11 +1300

    [update] automatically generate api key in data initialization

--- backend/src/main/resources/db/migration/R__Init_data.sql
+++ backend/src/main/resources/db/migration/R__Init_data.sql
@@ -1,6 +1,7 @@
-INSERT INTO user (id, username, password, salt)
+INSERT INTO user (id, username, password, salt, api_key)
 SELECT 0,
        'root',
        'a57b7e53b7253a4ab71eac631647f61f',
-       'fYMtZ=DHj^$G'
+       'fYMtZ=DHj^$G',
+       'AK-' || lower(hex(randomblob(18)))
     WHERE NOT EXISTS (SELECT 1 FROM user WHERE id = 0);
